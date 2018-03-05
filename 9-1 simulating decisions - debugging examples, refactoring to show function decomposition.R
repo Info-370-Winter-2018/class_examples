0d144a949e2b35f6e0464133a6139d4778540e3f
Greg L Nelson <glnelson@uw.edu>
2018-03-05 13:06:22 -0800

    quick reorg for polish

--- 9-1 simulating decisions - debugging examples, refactoring to show function decomposition.R
+++ 9-1 simulating decisions - debugging examples, refactoring to show function decomposition.R
@@ -1,4 +1,30 @@
+profit_monte_carlo_given_choice_debug <- function(ads_to_buy){
+  # the price of ads fluctuates with the market
+  # we model these price fluctuations with a random value
+  browser()
+  output <- tribble( ~ads_to_buy, ~ad_cost, ~number_of_ads, 
+                     ~expected_sales_per_ad, ~profit)
+  output <- output %>% add_row(ads_to_buy=ads_to_buy)
+  output <- output %>%  mutate(
+    ad_cost = 9 + runif(1, -2, 6),
+    number_of_ads = ads_to_buy,
+    expected_sales_per_ad = 12*number_of_ads,
+    profit = expected_sales_per_ad - (ad_cost*number_of_ads)
+  )
+  
+  # residual_term <- resample(1, your_models_residuals)
+  # expected_sales_per_ad <- predict(model, 
+  #             newdata=(tibble for this one simulation)) + residual_term
+  
+  return(output)
+}
 
+profit_monte_carlo_given_choice_debug(0)
+profit_monte_carlo_given_choice_debug(1)
+
+
+# you can refactor as a part of debugging;
+# here we isolated one value
 random_ad_cost_fun <- function(mean){
   mean + runif(1, -2, 6)
 }
@@ -7,11 +33,16 @@ profit_monte_carlo_given_choice <- function(ads_to_buy){
   # the price of ads fluctuates with the market
   # we model these price fluctuations with a random value
   #browser()
+  
   ad_cost <- random_ad_cost_fun(mean=9)
+  # original ad_cost line
+  # ad_cost <- 9 + runif(1, -2, 6)
+  
   number_of_ads <- ads_to_buy
+  
   expected_sales_per_ad <- 12*number_of_ads
   # residual_term <- resample(1, your_models_residuals)
- # expected_sales_per_ad <- predict(model, 
+  # expected_sales_per_ad <- predict(model, 
   #             newdata=(tibble for this one simulation)) + residual_term
   
   profit <- expected_sales_per_ad - (ad_cost*number_of_ads)
@@ -19,6 +50,7 @@ profit_monte_carlo_given_choice <- function(ads_to_buy){
 }
 
 #refactor to see that the profit is a factor of the sales
+# this makes function composition more explicit
 profit_monte_carlo_given_choice <- function(ads_to_buy){
   # the price of ads fluctuates with the market
   # we model these price fluctuations with a random value
@@ -58,29 +90,7 @@ profit_monte_carlo_given_choice_buggy <- function(ads_to_buy){
 profit_monte_carlo_given_choice_buggy(1)
 
 
-profit_monte_carlo_given_choice_debug <- function(ads_to_buy){
-  # the price of ads fluctuates with the market
-  # we model these price fluctuations with a random value
-  browser()
-  output <- tribble( ~ad_cost, ~number_of_ads, 
-                     ~expected_sales_per_ad, ~profit)
-  output <- output %>% add_row()
-  output <- output %>%  mutate(
-    ad_cost = 9 + runif(1, -2, 6),
-    number_of_ads = ads_to_buy,
-    expected_sales_per_ad = 12*number_of_ads,
-    profit = expected_sales_per_ad - (ad_cost*number_of_ads)
-  )
-  
-  # residual_term <- resample(1, your_models_residuals)
-  # expected_sales_per_ad <- predict(model, 
-  #             newdata=(tibble for this one simulation)) + residual_term
-  
-  return(output)
-}
 
-profit_monte_carlo_given_choice_debug(0)
-profit_monte_carlo_given_choice_debug(1)
 results <- tribble (~choice, ~mean_profit)
 
 for (ads_to_buy in seq(from=0, to=20, by=1)){
